d9abc080978c8b556a022d51a031135a94b58843
GustMangust <gust.mangust@gmail.com>
2022-04-09 04:43:50 +0300

    feat: changed logic

--- DeliveryOriginal/DeliveryOriginal.Api/app.js
+++ DeliveryOriginal/DeliveryOriginal.Api/app.js
@@ -8,10 +8,10 @@ const { Client } = require("pg");
 
 
 const credentials = {
-    user: 'lwbrstmokfkeih',
-    host: 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
-    database: 'dsgq5lbr01qlf',
-    password: '3d49ead7be2706fbc705f562944e6be2d18f5a43913b813dd0704cbdfb68bca6',
+    user: 'hvnceixkfztruv',
+    host: 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
+    database: 'd22ko0vn8djg',
+    password: '6836d1d29b64c2a9fc901c7428921b569cf183d5236fc3718a9502acf5334adf',
     port: 5432,
      ssl: { rejectUnauthorized: false }
   }
@@ -27,11 +27,24 @@ function getClient(){
     
     return client;
 }
+app.get("/Dish/GetAll",async function(req, res){
+    
+    const client = getClient();
+
+    let {rows} = await client.query('select *  from getAll(null::"Dish")');
+    
+    await client.end();
+    
+    res.send(rows);
+});
+
 app.get("/Dish/Get",async function(req, res){
     
     const client = getClient();
 
-    let {rows} = await client.query('select * from "Dish"');
+    let {Id} = req.query;
+
+    let {rows} = await client.query(`select * from getById(null::"Dish", ${Id})`);
     
     await client.end();
     
@@ -42,96 +55,151 @@ app.post("/Dish/Add", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Name, Cost} = req.body;
+    try{
 
-    const client = getClient();
+        const {Name, Cost} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`insert into "Dish" ("Name","Cost") values ($1,$2)`,[Name,Cost])
+        
+    } catch(err){
 
-    await client.query(`insert into "Dish" ("Name","Cost") values ($1,$2)`,[Name,Cost])
-    
-    res.send(req.body);
+    }
 });
 app.put("/Dish/Update", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Name,Cost, Id} = req.body;
-    
-    console.log(`${Name} + ${Cost} + ${Id} + "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"`);
+    try{
 
-    const client = getClient();
+        const {Name,Cost, Id} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`update "Dish" set "Name"=$1,"Cost"=$2 where "Id" = $3`,[Name,Cost,Id])
+        
+    } catch(err){
 
-    await client.query(`update "Dish" set "Name"=$1,"Cost"=$2 where "Id" = $3`,[Name,Cost,Id])
-    
-    res.send(req.body);
+    }
 });
 app.delete("/Dish/Delete", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Id} = req.body;
+    try{
 
+        const {Id} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`delete from "Dish" where "Id" = $1`,[Id])
+    } catch(err){
+
+    }
+});
+
+app.get("/User/GetAll",async function(req, res){
+    
     const client = getClient();
 
-    await client.query(`delete from "Dish" where "Id" = $1`,[Id])
+    let {rows} = await client.query('select * from getAll(null::"User")');
     
-    res.send(req.body);
+    await setUserFields(rows, client);
+
+    await client.end();
+    
+    res.send(rows);
 });
 
 app.get("/User/Get",async function(req, res){
     
     const client = getClient();
 
-    let {rows} = await client.query('select * from "User"');
+    let {Id} = req.query;
+
+    let {rows} = await client.query(`select * from getById(null::"User", ${Id})`);
     
+    await setUserFields(rows, client);
+
     await client.end();
     
     res.send(rows);
-});
+}); 
 
 app.post("/User/Add", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Login, Password, FullName, Role} = req.body;
+    try {
 
-    const client = getClient();
-
-    await client.query(`insert into "User" ("Login","Password", "FullName", "Role") values ($1,$2,$3,$4)`,[Login, Password, FullName, Role]);
-    
-    res.send(req.body);
+        const {Login, Password, FullName, Role} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`insert into "User" ("Login","Password", "FullName", "RoleId") values ($1,$2,$3,$4)`,[Login, Password, FullName, Role]);
+        
+        res.sendStatus(200);
+    } catch(err){
+        res.send("пизда рулю");
+    }
 });
 app.put("/User/Update", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Login, Password, FullName, Role, Id} = req.body;
+    try{
 
-    const client = getClient();
-
-    await client.query(`update "User" set "Login"=$1,"Password"=$2, "FullName" = $3, "Role"=$4 where "Id" = $5`,[Login, Password, FullName, Role, Id])
-    
-    res.send(req.body);
+        const {Login, Password, FullName, Role, Id} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`update "User" set "Login"=$1,"Password"=$2, "FullName" = $3, "Role"=$4 where "Id" = $5`,[Login, Password, FullName, Role, Id])
+        
+        res.sendStatus(200);
+    } catch(err){
+        res.send("пизда рулю");
+    }   
 });
 app.delete("/User/Delete", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
-    
-    const {Id} = req.body;
-
-    const client = getClient();
-
-    await client.query(`delete from "User" where "Id" = $1`,[Id])
-    
-    res.send(req.body);
+    try{
+        const {Id} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`delete from "User" where "Id" = $1`,[Id])
+        res.sendStatus(200);
+    } catch(err){
+        res.send("пизда рулю");
+    }        
 });
+app.get("/Order/GetAll",async function(req, res){
+    
+    const client = getClient();
+    
+    let {rows} = await client.query('select * from getById(null::"Order")');
+    
+    await setOrderFields(rows, client);
+    
+    await client.end();
+
+    res.send(rows);
+});
+
 app.get("/Order/Get",async function(req, res){
     
     const client = getClient();
 
-    let {rows} = await client.query('select * from "Order"');
+    let {Id} = req.query;
+
+    let {rows} = await client.query(`select * from getById(null::"Order",${Id})`);
     
+    await setOrderFields(rows,client);
+
     await client.end();
-    
+
     res.send(rows);
 });
 
@@ -139,72 +207,119 @@ app.post("/Order/Add", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Status, Date, Address, CustomerId} = req.body;
-
-    const client = getClient();
-
-    await client.query(`insert into "Order" ("Status","Date", "Address", "CustomerId") values ($1,$2,$3,$4)`,[Status,Date, Address, CustomerId]);
+    try{
+        
+        const {Status, Date, Address} = req.body;
+        
+        const CustomerId = req.body.Customer.Id;
+        
+        const client = getClient();
+        
+        await client.query(`insert into "Order" ("Status","Date", "Address", "CustomerId") values ($1,$2,$3,$4)`,[Status,Date, Address, CustomerId]);
+        
+    } catch(err){
+        
+    }
     
-    res.send(req.body);
 });
 app.put("/Order/Update", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {EmployeeId, Id} = req.body;
+    try{
 
-    const client = getClient();
+        const {Id, SubmittedAt, Status, Address} = req.body;
+        
+        const EmployeeId = req.body.CurrentEmployee.Id; 
 
-    await client.query(`update "Order" set "EmployeeId"=$1 where "Id" = $2`,[EmployeeId, Id])
-    
-    res.send(req.body);
+        const CustomerId = req.body.Customer.Id; 
+
+        const client = getClient();
+        
+        await client.query(`select updateOrderByOrderId($1,$2,$3,$4,$5,$6)`,[Id, SubmittedAt, Status, Address, EmployeeId, CustomerId]);
+        
+        const order = req.body;
+
+        await updateOrderedDishes(order,client);
+
+        res.sendStatus(200);
+        
+    } catch(err){
+        
+    }    
 });
+
 app.delete("/Order/Delete", jsonParser, async function (req, res) {
     
     if(!req.body) return res.sendStatus(400);
     
-    const {Id} = req.body;
-
-    const client = getClient();
-
-    await client.query(`delete from "Order" where "Id" = $1`,[Id])
-    
-    res.send(req.body);
-});
-app.get("/OrderedDish/Get",async function(req, res){
-    
-    const client = getClient();
-
-    let {rows} = await client.query('select * from "OrderedDish"');
-    
-    await client.end();
-    
-    res.send(rows);
+    try{
+        
+        const {Id} = req.body;
+        
+        const client = getClient();
+        
+        await client.query(`delete from "Order" where "Id" = $1`,[Id])
+        
+    }catch(err){
+        
+    }    
 });
 
-app.post("/OrderedDish/Add", jsonParser, async function (req, res) {
-    
-    if(!req.body) return res.sendStatus(400);
-    
-    const {DishId, OrderId} = req.body;
+async function setOrderFields(rows, client){
 
-    const client = getClient();
+    await setDishes(rows, client);
+    await setUser(rows, client);
+}
 
-    await client.query(`insert into "OrderedDish" ("OrderId","DishId") values ($1,$2)`,[OrderId, DishId]);
-    
-    res.send(req.body);
-});
-app.delete("/OrderedDish/Delete", jsonParser, async function (req, res) {
-    
-    if(!req.body) return res.sendStatus(400);
-    
-    const {Id} = req.body;
+async function setDishes(rows, client){
 
-    const client = getClient();
+    for(const order of rows)  {
+        order.Dishes = (await client.query(`select * from getDishesByOrderId(${order.Id})`)).rows;
+    };
 
-    await client.query(`delete from "OrderedDish" where "Id" = $1`,[Id])
+    return rows;
+}
+
+async function setUser(rows, client){
+
+    for(const order of rows)  {
+
+        order.Customer = (await client.query(`select * from getById(null::"User", ${order.CustomerId})`)).rows;
+        
+        order.CurrentEmployee = (await client.query(`select * from getById(null::"User", ${order.EmployeeId})`)).rows;
+
+        delete order.EmployeeId;
+
+        delete order.CustomerId;
+    };
+
+    return rows;
+}
+
+async function setUserFields(rows, client){
+    await setRole(rows, client);
+}
+
+async function setRole(rows, client){
+
+    for(const user of rows)  {
+        user.Role = (await client.query(`select * from getById(null::"Role",${user.RoleId})`)).rows;
+        
+        delete user.RoleId;
+    };
+
+    return rows;
+}
+
+async function updateOrderedDishes(order, client){
     
-    res.send(req.body);
-});
-
+    let {Id} = order;
+    
+    await client.query(`select deleteOrderedDishesByOrderId(${Id})`); 
+    
+    for(const dish of order.Dishes){
+        await client.query(`select insertOrderedDish(${Id}, ${dish.Id})`)
+    }
+}
 app.listen(process.env.PORT || 3000);
