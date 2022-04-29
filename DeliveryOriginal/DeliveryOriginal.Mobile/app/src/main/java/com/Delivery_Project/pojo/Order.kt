8f73749103215097d627a9e533693691a2a7832d
DESKTOP-97687CJ\Valson <harudareaper@gmail.com>
2022-04-30 00:48:29 +0300

    feat: added cook

--- DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/pojo/Order.kt
+++ DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/pojo/Order.kt
@@ -1,12 +1,14 @@
 package com.Delivery_Project.pojo
 
+import java.io.Serializable
+
 data class Order(
     val Address: String,
-    val CurrentEmployee: Any,
+    val CurrentEmployee: User,
     val Customer: User,
-    val Dishes: List<Dish>,
+    val Dishes: ArrayList<Dish>,
     val Id: Int,
     val PhoneNumber: String,
     val Status: Int,
     val SubmittedAt: String
-)
+):Serializable
--- DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/ui/ui/fragment/TakenCookOrdersFragment.kt
+++ DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/ui/ui/fragment/TakenCookOrdersFragment.kt
@@ -1,14 +1,33 @@
 package com.Delivery_Project.ui.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.Delivery_Project.R
+import com.Delivery_Project.adapter.AvailableCookOrderAdapter
+import com.Delivery_Project.adapter.TakenCookOrderAdapter
+import com.Delivery_Project.databinding.FragmentAvailableCookOrdersBinding
+import com.Delivery_Project.databinding.FragmentTakenCookOrdersBinding
+import com.Delivery_Project.factory.OrderViewModelFactory
+import com.Delivery_Project.repository.OrderRepository
+import com.Delivery_Project.retrofit.InterfaceAPI
+import com.Delivery_Project.ui.ui.activity.CookActivity
+import com.Delivery_Project.ui.ui.activity.LoginActivity
+import com.Delivery_Project.utility.SharedPreferencesUtility
+import com.Delivery_Project.viewModel.OrderViewModel
 
 class TakenCookOrdersFragment : Fragment() {
-
+    private val TAG = "HomeActivity"
+    private lateinit var binding: FragmentTakenCookOrdersBinding
+    lateinit var viewModel: OrderViewModel
+    private val interfaceAPI = InterfaceAPI.getInstance()
+    val adapter = TakenCookOrderAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +37,29 @@ class TakenCookOrdersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_taken_cook_orders, container, false)
+        val view: View  = inflater.inflate(R.layout.fragment_taken_cook_orders, container, false)
+        binding = FragmentTakenCookOrdersBinding.inflate(layoutInflater, container, false)
+        val activity = activity as CookActivity
+        val user = activity.getUser()
+        binding.currentCookTakenOrder.text = user.Login
+        binding.cookLogoutTakenOrder.setOnClickListener {
+            logout()
+        }
+        viewModel = ViewModelProvider(this, OrderViewModelFactory(OrderRepository(interfaceAPI))).get(
+            OrderViewModel::class.java)
+        binding.recyclerviewTakenCookOrders.adapter = adapter
+        viewModel.orderList.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "onCreate: $it")
+            adapter.setDishListByStatusAndEmployee(it, 2, user)
+        })
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer {
+        })
+        viewModel.getOrder()
+        return binding.root
     }
 
+    fun logout(){
+        SharedPreferencesUtility.deleteUser(requireContext())
+        startActivity(Intent(requireContext(), LoginActivity::class.java ))
+    }
 }
