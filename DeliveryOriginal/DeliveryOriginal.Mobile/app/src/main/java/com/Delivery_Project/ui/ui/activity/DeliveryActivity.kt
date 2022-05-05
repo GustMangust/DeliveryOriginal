c1e595c85cea1b1093a74b8b8b9c26a093b696e0
GustMangust <gust.mangust@gmail.com>
2022-05-05 23:18:13 +0300

    added: map

--- DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/ui/ui/activity/DeliveryActivity.kt
+++ DeliveryOriginal/DeliveryOriginal.Mobile/app/src/main/java/com/Delivery_Project/ui/ui/activity/DeliveryActivity.kt
@@ -1,30 +1,49 @@
 package com.Delivery_Project.ui.ui.activity
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.viewpager.widget.ViewPager
 import com.Delivery_Project.R
 import com.Delivery_Project.adapter.CookViewPagerAdapter
 import com.Delivery_Project.pojo.User
-import com.Delivery_Project.ui.ui.fragment.AvailableCookOrdersFragment
-import com.Delivery_Project.ui.ui.fragment.AvailableDeliveryOrdersFragment
-import com.Delivery_Project.ui.ui.fragment.TakenCookOrdersFragment
-import com.Delivery_Project.ui.ui.fragment.TakenDeliveryOrdersFragment
+import com.Delivery_Project.ui.ui.fragment.*
+import com.Delivery_Project.utility.SharedPreferencesUtility
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.tabs.TabLayout
+import java.util.*
 
 class DeliveryActivity: AppCompatActivity() {
     private lateinit var user: User
+    private val permissionId = 2
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivery)
         setUpTabs()
         user  = intent.getSerializableExtra("User") as User
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocation()
     }
     private fun setUpTabs(){
         val adapter = CookViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(AvailableDeliveryOrdersFragment(),"Available orders")
         adapter.addFragment(TakenDeliveryOrdersFragment(), "Taken orders")
+        adapter.addFragment(MapFragment(), "Map")
         val viewPager = findViewById<ViewPager>(R.id.deliveryViewPager)
         viewPager.adapter = adapter
         val tabs = findViewById<TabLayout>(R.id.deliveryTabs)
@@ -32,7 +51,69 @@ class DeliveryActivity: AppCompatActivity() {
 
     }
 
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    fun getLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task  ->
+                    val location: Location? = task.result
+                    if (location != null) {
+                        SharedPreferencesUtility.saveLocation(location,this)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            permissionId
+        )
+    }
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == permissionId) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            }
+        }
+    }
     fun getUser() : User{
-        return user;
+        return user
     }
 }
